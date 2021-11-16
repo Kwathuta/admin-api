af5ac30f3701a65febf70e5f951078d003b09701
ken <ken.mbira@student.moringaschool.com>
2021-11-16 14:55:30 +0300

    add permissions to create user endpoiint

--- fuzu_pay/settings.py
+++ fuzu_pay/settings.py
@@ -49,9 +49,13 @@ INSTALLED_APPS = [
     'phonenumber_field',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,6 +64,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 ROOT_URLCONF = 'fuzu_pay.urls'
 
--- superadmin/views.py
+++ superadmin/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 from superadmin.serializers import *
@@ -13,6 +14,7 @@ class UserView(APIView):
     Args:
         generics ([type]): [description]
     """
+    permission_classes = [IsAuthenticated]
     def post(self,request,format=None):
         data = {}
         serializer = UserCreationSerializer(data=request.data)
