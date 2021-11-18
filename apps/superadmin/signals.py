f64116bf2c4ad3c450f315ebad7767178b10bd43
ken <ken.mbira@student.moringaschool.com>
2021-11-18 11:40:32 +0300

    change location of the create roles function

--- apps/superadmin/signals.py
+++ apps/superadmin/signals.py
@@ -9,13 +9,12 @@ from apps.superadmin.models import *
 
 @receiver(post_save, sender=User)
 def create_associate_tables(sender, instance, created, **kwargs):
+    if len(Role.objects.all()) < 3:
+        Role.objects.create(name="super_admin")
+        Role.objects.create(name="human_resources")
+        Role.objects.create(name="subordinate_staff")
     if created:
         Profile.objects.create(employee=instance)
         # EmploymentInformation.objects.create(employee=instance)
         # PaymentInformation.objects.create(employee=instance)
         # EmergencyInformation.objects.create(employee=instance)
-
-if len(Role.objects.all()) < 3:
-    Role.objects.create(name="super_admin")
-    Role.objects.create(name="human_resources")
-    Role.objects.create(name="subordinate_staff")
