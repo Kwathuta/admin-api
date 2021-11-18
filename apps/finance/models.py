53c17ce0f9196c83d9b3ea517975c877e9995884
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-11-18 21:03:58 +0300

    created approve table

--- apps/finance/models.py
+++ apps/finance/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Approve(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    amount=models.DecimalField(decimal_places=2, max_digits=40)
+
+    def __str__(self):
+        return self.name
