ff49e041a491dc27b7e19be1fd373d2ab351f00c
ken <ken.mbira@student.moringaschool.com>
2021-11-18 13:10:16 +0300

    configuring cors headers

--- settings/base.py
+++ settings/base.py
@@ -49,11 +49,13 @@ INSTALLED_APPS = [
     'drf_yasg',
     'rest_framework.authtoken',
     'rest_framework',
+    'corsheaders',
 ]
 
 LOGIN_REDIRECT_URL = ''
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -104,6 +106,9 @@ DATABASES = {
 
 #user model
 AUTH_USER_MODEL = 'superadmin.User'
+
+#CORS Headers configuration
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Password validation
