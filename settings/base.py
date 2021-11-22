6f61481cee798e4bc58e9fcc25af83b15a548cfe
ken <ken.mbira@student.moringaschool.com>
2021-11-22 10:15:55 +0300

    add reset password functionality

--- settings/base.py
+++ settings/base.py
@@ -76,6 +76,12 @@ REST_FRAMEWORK = {
     ]
 }
 
+EMAIL_USE_TLS = os.environ['EMAIL_USE_TLS']
+EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_PORT = os.environ['EMAIL_PORT']
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+
 ROOT_URLCONF = 'FUZU_PAY.urls'
 
 TEMPLATES = [
